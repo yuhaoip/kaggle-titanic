56a69f278831a69faf10f1291490552a4912f7af
Yang Yuhao <yuhao107@gmail.com>
2015-12-03 14:28:03 +0800

    feature engineering - phase1
    Add 'FamilyId', 'Title' features, then choosed the five best features
    ['Pclass', 'Sex', 'Fare', 'Title', FamilyId'] according to
    .SelectKBest().scores_, to train model.

--- 00-titanic/titanic1.py
+++ 00-titanic/titanic1.py
@@ -2,7 +2,8 @@
 
 import numpy as np
 import pandas as pd
-from sklearn.metrics.classification import accuracy_score
+import operator
+import matplotlib.pyplot as plt
 
 
 train = pd.read_csv('./input/train.csv')
@@ -13,6 +14,7 @@ predictorsLabel = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
 def harmonizeData(titanic):
     
     titanic['Age'] = titanic['Age'].fillna(titanic['Age'].median())
+    
     
     titanic.loc[titanic['Sex']=='male', 'Sex'] = 0
     titanic.loc[titanic['Sex']=='female', 'Sex'] = 1
@@ -27,28 +29,116 @@ def harmonizeData(titanic):
     return titanic
 
 
-"""
-# def addFeature(titanic):
-#     
-#     return titanic 
-"""  
-
- 
-def createSubmission(alg, trainData, testData, predictors, filename):
+# Extract title from a certain name and mapped to a id
+def extractTitle(name):
+    import re
+    # Extract the title by regular expression.
+    titleSearch = re.search(r'([A-Za-z]+)\.', name)
+    if titleSearch:
+        title = titleSearch.group(1)
+        return title  
+    return ""
+      
+# Combine lastName with familysize to get an unique Id 
+familyIdMapping = {}
+def getFamilyId(row):
     
-    alg.fit(trainData[predictors], trainData['Survived'])
-    predictions = alg.predict(testData[predictors])
+    lastName = row['Name'].split(',')[0]
+    familyId = '{0}{1}'.format(lastName, row['FamilySize'])
+    # Map the id to number
+    if familyId not in familyIdMapping:
+        if len(familyIdMapping)==0:
+            currentId = 1
+        else: # Get the maximun id and add 1
+            currentId = (max(familyIdMapping.items(), 
+                            key=operator.itemgetter(1))[1]+1)
+        familyIdMapping[familyId]=currentId
+    
+    return familyIdMapping[familyId]
+         
+    
+# Except for those existing features, try to build feature engineering
+def addFeatures(titanic):
+    # The title such as 'Countness.''Dr.' shows the identify. Should be useful.
+    # Map the titles into classified numerical value( combining the testData titles)
+    titles = titanic['Name'].apply(extractTitle)
+#     print 'The titels count: ', pd.value_counts(titles)
+    titleMapping = {'Mr':1, 'Miss':2, 'Ms':2, 'Mrs':3, 'Master':4, 'Dr':5,
+                  'Rev':6, 'Col':7, 'Major':7, 'Mlle':8, 'Mme':8,
+                  'Don':9, 'Lady':10, 'Countess':10, 'Jonkheer':10,
+                  'Sir':9, 'Capt':7, 'Dona':10}
+    
+    for k, v in titleMapping.items(): 
+        titles[titles==k] = v
+#     print 'The titles count: ', pd.value_counts(titles)
+    titanic['Titles'] = titles
+    
+    # Combine last name and family size to get a unique id;
+    # There are too many family ids, so compress all of the familyies
+    # under 3 members into one code.
+    titanic['FamilySize'] = titanic['SibSp']+titanic['Parch']
+    familyIds = titanic.apply(getFamilyId, axis=1)
+    familyIds[titanic['FamilySize']<2]=-1
+    titanic['FamilyId'] = familyIds
+    
+    # Now, think about the female prop in a family
+    
+    return titanic 
+
+
+# Choose the best K features
+def chooseBestFeatures(trainData, predictors=predictorsLabel, k=5):
+    
+    from sklearn.feature_selection import SelectKBest, f_classif
+    
+    selector = SelectKBest(f_classif, k)
+    selector.fit(trainData[predictors], trainData['Survived'])
+    scores = selector.scores_
+    print 'The scores are: ', scores
+    
+    # plot the feature-score bar to inspect
+    plt.bar(range(len(predictors)), scores)
+    plt.xticks(range(len(predictors)), predictors, rotation='vertical')
+    plt.show()
+    
+    
+
+def createSubmission(algorithms, trainData, testData, filename):
+    
+    predictions = []
+    for alg, predictors in algorithms:
+        alg.fit(trainData[predictors], trainData['Survived'])
+        test_predictions = alg.predict_proba(testData[predictors].astype(float))[:,1]
+        predictions.append(test_predictions)
+    
+    sumPredictions = np.array([0.0]*testData.shape[0])
+    for eachItem in predictions: 
+        sumPredictions += eachItem
+    
+    predictions = sumPredictions/len(predictions)
+    predictions[predictions<=.5] = 0
+    predictions[predictions>.5] = 1
+    
+    # Transfer np.ndarray to pd.core.series.Series
+    predictions = pd.Series(predictions)
+    # According to pre-insight in excel, 
+    # set['Pclass'=1&'Sex'=1 to 1, 'Pclass'=2&'Sex'=0&'Age'>18 to 0]
+    predictions[(testData['Pclass']==1) & (testData['Sex']==1)] = 1
+    predictions[(testData['Pclass']==2) & (testData['Sex']==0) & (testData['Age']>18)] = 0
+    predictions = predictions.astype(int)
     
     submission = pd.DataFrame({'PassengerId': testData['PassengerId'],
                                 'Survived': predictions})
-    
+     
     submission.to_csv(filename, index=False)
-    
- 
+        
+        
+        
+        
 # Below will test different algorithms to get an intuitive evaluation
 # They are: Logistic Regression, RandomForest, GBDT and SVM 
 # In last, try an ensemble method on combining two different algorithm 
-# 0.78788 --0.75120
+# 0.78788 --0.75120; 0.78563
 def predictByLogisticRegres(trainData, predictors=predictorsLabel):
     
     from sklearn.linear_model import LogisticRegression
@@ -61,7 +151,8 @@ def predictByLogisticRegres(trainData, predictors=predictorsLabel):
     
     print 'the score by Logistic Regression is: ', scores.mean()
 
-# 0.82379 --0.76077
+
+# 0.82379 --0.76077; 0.81818
 def predictByRandomForest(trainData, predictors=predictorsLabel):
     
     from sklearn.ensemble import RandomForestClassifier
@@ -75,7 +166,7 @@ def predictByRandomForest(trainData, predictors=predictorsLabel):
     print 'The score by Random Forest is: ', scores.mean()
     
 
-# 0.81936 -- 0.77990
+# 0.81936 -- 0.77990; 0.81817
 def predictByGradientBoosting(trainData, predictors=predictorsLabel):
     
     from sklearn.ensemble import GradientBoostingClassifier
@@ -89,7 +180,7 @@ def predictByGradientBoosting(trainData, predictors=predictorsLabel):
     print 'The score by GBDT is: ', scores.mean()
     
 
-# 0.79580  --0.77033
+# 0.79580  --0.77033; 0.80472
 def predictBySVM(trainData, predictors=predictorsLabel):  
     
     from sklearn.svm import SVC
@@ -117,6 +208,9 @@ def getEnsembleResult(PredictionsList, method='averagingProbs'):
 
 # Think: How to apply this ensemble method to the whole traing set?
 # 0.80920 --0.78469
+# 0.83053 --0.79904(predictors=['Pclass','Sex','Titles','FamilyId','Fare'])
+# 0.83389 --(predictors=['Pclass','Sex','Titles','FamilyId','Fare','Embarked'])
+# 0.81033 --(predictors=['Pclass','Sex','Titles','FamilyId','Fare','Embarked','Age'])
 def predictByEnsemble(trainData, 
                       predictors1=predictorsLabel, 
                       predictors2=predictorsLabel):
@@ -138,7 +232,7 @@ def predictByEnsemble(trainData,
         for alg, predictors in algorithms:
             
             alg.fit(trainData[predictors].iloc[train,:],trainTarget)
-            # 要使用.predict_proba()得到各个类的概率; 因此，SVC中的probability=True
+            # here, need .proba() method to caculate probability.
             # .astype(float)is necessary.
             testPredictions = alg.predict_proba(trainData[predictors].iloc[test,:].astype(float))[:,1]
             bothPredictions.append(testPredictions)
@@ -152,10 +246,11 @@ def predictByEnsemble(trainData,
     # concatenate the seperate predictions
     predictions = np.concatenate(predictions, axis=0)
     accuracy = sum(predictions[predictions == trainData['Survived']])/len(predictions)
-    
+
     print 'The accuracy by ensemble of GBDT&SVM is: ', accuracy
 
-        
+ 
+   
     
     
     
